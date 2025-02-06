b563932a7ea25629b7c803e17d54fc5c40805fc3
YunbinGil <bq0403@konkuk.ac.kr>
2025-02-06 14:50:06 +0900

    Test Setting

---
+++ CS449-SOS/src/test/java/com/YunbinGil/sos/SosGameTest.java
@@ -0,0 +1,13 @@
+package com.YunbinGil.sos;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SosGameTest {
+    @Test
+    void testExample() {
+        int a = 5;
+        int b = 10;
+        assertEquals(15, a + b);
+    }
+}
